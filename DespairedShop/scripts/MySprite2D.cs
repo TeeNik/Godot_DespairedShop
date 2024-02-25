d8961bc3d67d55c27ba6c391168fccd5dae113ea
TeeNik <teenik3315@gmail.com>
2024-02-25 19:23:43 +0100

    tutorial on basic input and movement

--- DespairedShop/scripts/MySprite2D.cs
+++ DespairedShop/scripts/MySprite2D.cs
@@ -2,8 +2,34 @@ using Godot;
 
 public partial class MySprite2D : Sprite2D
 {
+    private int _speed = 400;
+    private float _anglularSpeed = Mathf.Pi;
+    
     public MySprite2D()
     {
         GD.Print("Hello World!");
+    }
+    
+    public override void _Process(double delta)
+    {
+        var direction = 0.0f;
+        if (Input.IsActionPressed("ui_left"))
+        {
+            direction -= 1;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            direction += 1;
+        }
+        
+        Rotation += _anglularSpeed * direction * (float)delta;
+
+        var velocity = Vector2.Zero;
+        if (Input.IsActionPressed("ui_up"))
+        {
+            velocity = Vector2.Up.Rotated(Rotation) * _speed;
+        }
+        
+        Position += velocity * (float)delta;
     }
 }
