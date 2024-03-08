7a5ca7e6742ba17038d8ae9782e592c7ee618e8b
TeeNik <teenik3315@gmail.com>
2024-03-08 20:34:37 +0100

    MobScene for tutorial

---
+++ SquashTheCreeps/scenes/mob/Mob.cs
@@ -0,0 +1,37 @@
+using Godot;
+
+public partial class Mob : CharacterBody3D
+{
+    [Export] 
+    public int MinSpeed { get; set; } = 10;
+    [Export] 
+    public int MaxSpeed { get; set; } = 15;
+
+    private VisibleOnScreenEnabler3D _visibleOnScreenEnabler;
+
+    public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+    {
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+        RotateY((float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4));
+
+        int speed = GD.RandRange(MinSpeed, MaxSpeed);
+        Velocity = Vector3.Forward * speed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+    }
+
+    public override void _Ready()
+    {
+        _visibleOnScreenEnabler = GetNode<VisibleOnScreenEnabler3D>("VisibleOnScreenEnabler3D");
+        _visibleOnScreenEnabler.ScreenExited += OnScreenExited;
+    }
+
+    private void OnScreenExited()
+    {
+        QueueFree();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+}
