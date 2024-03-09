eba2a9233eadb13b68f4991aa511ec19e463d2c0
TeeNik <teenik3315@gmail.com>
2024-03-09 01:48:51 +0100

    spawn mobs for tutorial

---
+++ SquashTheCreeps/scenes/main/Main.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	private Timer _mobTimer;
+	private PathFollow3D _mobSpawnLocation;
+	private Player _player;
+	
+	public override void _Ready()
+	{
+		_mobTimer = GetNode<Timer>("MobTimer");
+		_mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		_player = GetNode<Player>("Player");
+		
+		_mobTimer.Timeout += OnMobTimerTimeout;
+	}
+
+	private void OnMobTimerTimeout()
+	{
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		_mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = _player.Position;
+		mob.Initialize(_mobSpawnLocation.Position, playerPosition);
+		
+		AddChild(mob);
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
