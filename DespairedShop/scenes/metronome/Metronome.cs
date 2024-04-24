aedd000d4932264f584654567c3a32d7b236c0b2
TeeNik <teenik3315@gmail.com>
2024-04-25 00:02:07 +0200

    spawn beats

--- DespairedShop/scenes/metronome/Metronome.cs
+++ DespairedShop/scenes/metronome/Metronome.cs
@@ -4,20 +4,30 @@ using System;
 public partial class Metronome : Control
 {
 	[Export] public int BPM = 130;
+	[Export] public float BeatLifetime = 3;
 	[Export] public PackedScene BeatScene;
 	
 	private Timer _beatTimer;
+	private float _beatPeriod;
+	private Control _beatSpawmPoint;
+	private Control _beatTargetPoint;
 	
 	public override void _Ready()
 	{
+		_beatPeriod = 60.0f / BPM;
 		_beatTimer = GetNode<Timer>("BeatTimer");
+		_beatSpawmPoint = GetNode<Control>("BeatSpawnPoint");
+		_beatTargetPoint = GetNode<Control>("BeatTargetPoint");
 		
 		_beatTimer.Timeout += OnBeatTimerTimeout;
+		_beatTimer.Start(_beatPeriod);
 	}
 
 	private void OnBeatTimerTimeout()
 	{
-		
+		GD.Print("Beat");
+		Beat beat = BeatScene.Instantiate<Beat>();
+		beat.InitBeat(BeatLifetime, _beatSpawmPoint.GlobalPosition, _beatTargetPoint.GlobalPosition);
 	}
 
 	public override void _Process(double delta)
