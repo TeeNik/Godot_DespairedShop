5404f3275f9b5bcb71b663dafcf78c707232a3ff
TeeNik <teenik3315@gmail.com>
2024-04-28 03:33:07 +0200

    spawn shots in slots

--- DespairedShop/scenes/metronome/Metronome.cs
+++ DespairedShop/scenes/metronome/Metronome.cs
@@ -3,9 +3,10 @@ using System;
 
 public partial class Metronome : Control
 {
-	[Export] public int BPM = 130;
-	[Export] public float BeatLifetime = 3;
-	[Export] public PackedScene BeatScene;
+	[Export] private int _bpm = 130;
+	[Export] private float _beatLifetime = 3;
+	[Export] private float _beatHitThreshold = 0.1f;
+	[Export] private PackedScene _beatScene;
 	
 	[Export] private Control _beatsParent;
 	[Export] private Control _beatSpawnPoint;
@@ -13,10 +14,21 @@ public partial class Metronome : Control
 	
 	private Timer _beatTimer;
 	private float _beatPeriod;
+	private float _currentBeatTime;
+
+	private static Metronome _instance;
+
+	public static Metronome Get()
+	{
+		return _instance;
+	}
 	
 	public override void _Ready()
 	{
-		_beatPeriod = 60.0f / BPM;
+		_instance = this;
+		
+		_beatPeriod = 60.0f / _bpm;
+		_currentBeatTime = _beatPeriod;
 		_beatTimer = GetNode<Timer>("BeatTimer");
 		
 		_beatTimer.Timeout += OnBeatTimerTimeout;
@@ -25,14 +37,21 @@ public partial class Metronome : Control
 
 	private void OnBeatTimerTimeout()
 	{
-		GD.Print("Beat");
-		Beat beat = BeatScene.Instantiate<Beat>();
+		_currentBeatTime = _beatPeriod;
+		Beat beat = _beatScene.Instantiate<Beat>();
 		_beatsParent.AddChild(beat);
 		beat.SetPosition(_beatSpawnPoint.Position);
-		beat.InitBeat(BeatLifetime, _beatSpawnPoint.Position, _beatTargetPoint.Position);
+		beat.InitBeat(_beatPeriod, _beatSpawnPoint.Position, _beatTargetPoint.Position);
 	}
 
 	public override void _Process(double delta)
 	{
+		_currentBeatTime -= (float)delta;
+	}
+
+	public bool IsHitBeat()
+	{
+		GD.Print(_currentBeatTime);
+		return _currentBeatTime < _beatHitThreshold;
 	}
 }
--- DespairedShop/scenes/player/Player.cs
+++ DespairedShop/scenes/player/Player.cs
@@ -1,16 +1,20 @@
 using Godot;
-using System;
+using Godot.Collections;
 
 public partial class Player : Area2D
 {
-	[Signal]
-	public delegate void HitEventHandler();
+	[Signal] public delegate void HitEventHandler();
 	
-	[Export] 
-	public int Speed { get; set; } = 400;
+	[Export] private int _speed = 400;
+	[Export] private Node2D _pivot;
+
+	[Export] private PackedScene _shotScene;
 
 	private Vector2 _screenSize;
 	private CollisionShape2D _collisionShape2D;
+	
+	private Array<Node> _shotSlots = new ();
+	private Array<Node2D> _shots = new();
 	
 	public override void _Ready()
 	{
@@ -18,6 +22,14 @@ public partial class Player : Area2D
 		BodyEntered += OnBodyEntered;
 		
 		_collisionShape2D = GetNode<CollisionShape2D>("CollisionShape2D");
+
+		_shotSlots = _pivot.GetChildren();
+		foreach (var slot in _shotSlots)
+		{
+			Node2D shot = _shotScene.Instantiate<Node2D>();
+			_shots.Add(shot);
+			slot.AddChild(shot);
+		}
 		
 		//Hide();
 	}
@@ -52,14 +64,25 @@ public partial class Player : Area2D
 
 		if (velocity.Length() > 0)
 		{
-			velocity = velocity.Normalized() * Speed;
+			velocity = velocity.Normalized() * _speed;
 		}
 
 		Position += velocity * (float)delta;
 		Position = new Vector2(Mathf.Clamp(Position.X, 0, _screenSize.X), Mathf.Clamp(Position.Y, 0, _screenSize.Y));
 
+		_pivot.Rotate((float)delta);
 	}
-	
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton && @event.IsPressed())
+		{
+			Metronome.Get().IsHitBeat();
+			//string Text = Metronome.Get().IsHitBeat() ? "Hit" : "Miss";
+			//GD.Print(Text);
+		}
+	}
+
 	private void OnBodyEntered(Node2D body)
 	{
 		Hide();
