c81f9c6a614d24d684d105f868bdcf944f9af2c7
TeeNik <teenik3315@gmail.com>
2024-05-03 02:29:51 +0200

    indicator on shoot

--- DespairedShop/scenes/hud/HUD.cs
+++ DespairedShop/scenes/hud/HUD.cs
@@ -1,16 +1,31 @@
 using Godot;
 using System;
 
-public partial class Hud : Control
+public partial class HUD : CanvasLayer
 {
 	[Export] private Metronome _metronome;
 	[Export] private Player _player;
+
+	[Export] private TextureRect _playerShootIndicator;
+	[Export] private Color _hitBeatColor;
+	[Export] private Color _missBeatColor;
 	
 	public override void _Ready()
 	{
+		_player.Shoot += OnPlayerShoot;
 	}
 
-	public override void _Process(double delta)
+	private void OnPlayerShoot(bool isHitBeat)
 	{
+		ShowShootIndicator(isHitBeat);
+	}
+
+	private async void ShowShootIndicator(bool isHitBeat)
+	{
+		_playerShootIndicator.Show();
+		_playerShootIndicator.Modulate = isHitBeat ? _hitBeatColor : _missBeatColor;
+		
+		await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
+		_playerShootIndicator.Hide();
 	}
 }
